cd58e4befd948f1ad2a0994ed9ee43b5772dc069
Juan Olivas <juanolivas0693@gmail.com>
2020-09-12 13:30:00 +0300

    module_08 done

--- js/tasks.js
+++ js/tasks.js
@@ -5,28 +5,33 @@ const lightbox = document.querySelector(".lightbox");
 const btn = document.querySelector('[data-action="close-lightbox"]');
 
 images.forEach((item) => {
-  let li = document.createElement("li");
-  li.classList.add("gallery__item");
-  ulGallery.append(li);
-  let a = document.createElement("a");
-  a.classList.add("gallery__link");
-  a.setAttribute("href", item.original);
-  li.append(a);
   let img = document.createElement("img");
   img.classList.add("gallery__image");
   img.setAttribute("src", item.preview);
   img.setAttribute("data-source", item.original);
   img.setAttribute("alt", item.description);
+
+  let a = document.createElement("a");
+  a.classList.add("gallery__link");
+  a.setAttribute("href", item.original);
   a.append(img);
+
+  let li = document.createElement("li");
+  li.classList.add("gallery__item");
+  li.append(a);
+
+  ulGallery.append(li);
 });
 
 function handleUlClick(event) {
   console.log(event.target.dataset.source);
   event.preventDefault();
+
+  const lightboxImage = document.querySelector(".lightbox__image");
   if (event.target.nodeName === "IMG") {
     lightbox.classList.add("is-open");
-    lightbox.querySelector(".lightbox__image").src = event.target.src;
-    lightbox.querySelector(".lightbox__image").alt = event.target.alt;
+    lightboxImage.src = event.target.src;
+    lightboxImage.alt = event.target.alt;
   }
 }
 
